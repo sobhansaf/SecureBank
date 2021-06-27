dbff95509b74677f68ffda6d3f2b004666229f7a
sobhan <sobhan.saf79@gmail.com>
2021-06-27 20:04:07 +0430

    added a new file for shared date

---
+++ server/shared_data.py
@@ -0,0 +1,5 @@
+# letters is used to generate random strings for salt and authorization code
+letters = 'qwertyuiopasdfghjklzxcvbnm'
+letters += letters.upper()
+letters += '0123456789'
+letters += '~!@#$%^&*()_+=-{[}]|\\/:;"\'<>,.?'
