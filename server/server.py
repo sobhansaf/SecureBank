711392182367d92ab1040a0f0608b950258bb6d4
sobhan <sobhan.saf79@gmail.com>
2021-07-03 21:29:46 +0430

    added cryptography

--- server/server.py
+++ server/server.py
@@ -9,6 +9,9 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
 
 server_commands = {
     'Signup': signup.sign_up,
@@ -31,6 +34,18 @@ def read_private_key(private_key_address='private.pem'):
     return private_key
 
 
+def decrypt_asymmetric(encrypted, private_key):
+    res = private_key.decrypt(
+        encrypted,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    ))
+    return res
+
+
+fernet = None
 port = 23654
 host = '127.0.0.1'
 
@@ -40,14 +55,23 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((host, port))
 sock.listen()
 con, addr = sock.accept()
+private_key = read_private_key()
+
+encrypted_session_key = con.recv(1024)
+session_key = decrypt_asymmetric(encrypted_session_key, private_key)
+fernet = Fernet(session_key)
 
 while True:
     try:
-        data = con.recv(1024).decode().strip()
+        data = con.recv(1024)
+        if not data:
+            break
+        data = fernet.decrypt(data).decode().strip()
         data = data.split()
         res = server_commands[data[0]](*data[1:])
         res = list(map(str, res))
-        con.sendall(' '.join(res).encode())
+        res = fernet.encrypt((' '.join(res)).encode())
+        con.sendall(res)
     except KeyboardInterrupt:
         break
     except:
